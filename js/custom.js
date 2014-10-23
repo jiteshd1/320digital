be27247bce1dcca680d17188d0a2889051fcc584
jiteshd1 <jiteshd1@googlemail.com>
2014-10-23 23:00:38 +0100

    Updated index page

--- js/custom.js
+++ js/custom.js
@@ -47,6 +47,19 @@ function goGA( event, category, label ) {
 function initOnReady() {
 	
 	header = $("#main-header");
+	
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': ($target.offset().top - 80)
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
 
 }
 
